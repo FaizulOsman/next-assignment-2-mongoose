5ad4496d065a450ca325d80dd657ca428230f851
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-21 22:10:54 +0600

    database connect

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/book");
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
