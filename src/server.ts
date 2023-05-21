4365331ff68346db567b6e20f4e0723f92fda9da
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-21 23:16:38 +0600

    database connect

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 5000;
-const mongoose = require("mongoose");
+import app from "./app";
+import mongoose from "mongoose";
+
+const port: number = 5000;
 
 async function main() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/book");
     console.log("Database Connected");
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   } catch (error) {
     console.log(error);
   }
 }
 main();
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(port, () => {
-  console.log("Server is running on port 5000");
-});
